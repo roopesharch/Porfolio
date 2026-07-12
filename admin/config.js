2ddf201f30b01ee03f4058f5c1e10ad4524b1c7b
Roopesh S <42025266+roopesharch@users.noreply.github.com>
2026-07-13 04:10:25 +0530

    Update ADMIN_EMAIL in config.jsemail

--- admin/config.js
+++ admin/config.js
@@ -5,5 +5,5 @@ const CONFIG = {
     //  These keys are read only secured public keys, So its fine to display in repo
     SUPABASE_URL: 'https://nnzhjaxprvcchnssaaku.supabase.co',
     SUPABASE_ANON_KEY: 'sb_publishable_FP-hYnEK3RX86o_Ll5RgYQ_Ye_ug2iM',
-    ADMIN_EMAIL: 'your-actual-email@gmail.com' // Used for the RLS check
+    ADMIN_EMAIL: 'roopesharch@gmail.com' // Used for the RLS check
 };
