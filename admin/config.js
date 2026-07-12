22a312b727e2b4f031ceffffc0889799d166dc10
Roopesh S <42025266+roopesharch@users.noreply.github.com>
2026-07-13 04:09:45 +0530

    updated credentials

--- admin/config.js
+++ admin/config.js
@@ -3,7 +3,7 @@
  */
 const CONFIG = {
     //  These keys are read only secured public keys, So its fine to display in repo
-    SUPABASE_URL: 'https://irkywlnfizmlzkltbepk.supabase.co',
-    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlya3l3bG5maXptbHprbHRiZXBrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0NjM1ODIsImV4cCI6MjA4ODAzOTU4Mn0.10xYnVeD4SEdvpoCs9-ZU1cJRv8iCGLt5ELgbSWoFXE',
+    SUPABASE_URL: 'https://nnzhjaxprvcchnssaaku.supabase.co',
+    SUPABASE_ANON_KEY: 'sb_publishable_FP-hYnEK3RX86o_Ll5RgYQ_Ye_ug2iM',
     ADMIN_EMAIL: 'your-actual-email@gmail.com' // Used for the RLS check
 };
